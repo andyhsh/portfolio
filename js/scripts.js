4ff760d5cf3c6113da0846467f96b0fe9a1354cb
Andy Ho <89andy@gmail.com>
2017-05-24 01:03:29 +0800

    Change to flexbox. Now responsive.

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// $(window).scroll(function() {
+//
+//   // gets the current vertical position from the top of the window
+//   var scroll = $(this).scrollTop();
+//
+//   // gets the vertical position from the top of the window to #work
+//   var workSection = $('#work').offset().top;
+//
+//   var wHeight = $(window).height();
+//
+//   console.log('window', wHeight);
+//
+//   if(scroll > workSection) {
+//
+//     console.log()
+//
+//   }
+// });
